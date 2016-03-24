2be0f5b953eb8ab97b2466c6f88a46cef7e17555
Shengzhang Wen <coffeesherk@gmail.com>
2016-03-24 21:35:12 +0800

    remove checkmark select

--- GMImagePicker/GMCheckmarkView.h
+++ GMImagePicker/GMCheckmarkView.h
@@ -1,8 +1,5 @@
 #import <UIKit/UIKit.h>
 
 @interface GMCheckmarkView : UIControl
-
-@property (nonatomic, assign) BOOL selected;
-
 -(void) setSelected:(BOOL)selected animated:(BOOL)animated;
 @end
